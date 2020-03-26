0fad178c4e8c183728bf500f8c9e03832446f5f7
sgs22 <Samuel_Smith@outlook.com>
2020-03-26 16:38:41 +0000

    update lab 8

---
+++ lab7/part_a/application/model/model.php
@@ -0,0 +1,28 @@
+<?php
+class Model {
+	//Method to simulate the model data
+	public function model3D_info()
+	{
+		// Simulate the model's data
+		return array(
+			'model_1' => 'Coke Can 3D Image 1',
+			'image3D_1' => 'cokeGraphic',
+
+			'model_2' => 'Coke Can 3D Image 2',
+			'image3D_2' => 'fantaGraphic',
+
+			'model_3' => 'Sprite Bottle 3D Image 1',
+			'image3D_3' => 'lemonGraphic',
+
+			'model_4' => 'Sprite Bottle 3D Image 2',
+			'image3D_4' => 'spriteGraphic',
+
+			'model_5' => 'Dr Pepper Cup 3D Image 1',
+			'image3D_5' => 'cokeGraphic',
+
+			'model_6' => 'Dr Pepper Cup 3D Image 2',
+			'image3D_6' => 'cokeGraphic'
+		);
+	}
+}
+?>
---
+++ lab8/part_c/application/model/createJson.php
@@ -0,0 +1,23 @@
+<?php
+echo '{"users":[
+            {
+                "firstName":"Sam",
+                "lastName":"Smith",
+                "joined": {
+                    "month":"October",
+                    "day":21,
+                    "year":1996
+                }
+            },
+            {
+                "firstName":"Bob",
+                "lastName":"Gelfof",
+                "joined": {
+                    "month":"March",
+                    "day":20,
+                    "year":2019
+                }
+            }
+    ]}
+    ';
+?>
